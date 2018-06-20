8e9efc56085f25c8bd3242c03f22d306d4a67aa9
Brian Margolis <Brian.M.Margolis@gmail.com>
2018-06-20 16:36:49 -0500

    Modifications to prior work from experimenting

--- prior_work/bongjun/training_data_pairing.py
+++ prior_work/bongjun/training_data_pairing.py
@@ -2,26 +2,27 @@ import numpy as np
 import librosa
 import os
 import pickle
-import keras
 
 from collections import Counter
 import random
 
-def normalize_spectrogram(data):
+
+def normalize_spectrograms(spectrograms):
     '''
     data: (num_examples, num_freq_bins, num_time_frames)
     '''
-    m = np.mean(data, axis=(1,2))
+    m = np.mean(spectrograms, axis=(1, 2))
     m = m.reshape(m.shape[0], 1, 1)
-    std = np.std(data, axis=(1,2))
+    std = np.std(spectrograms, axis=(1, 2))
     std = std.reshape(std.shape[0], 1, 1)
-    
-    m_matrix = np.repeat(np.repeat(m, data.shape[1], axis=1), data.shape[2], axis=2)
-    std_matrix = np.repeat(np.repeat(std, data.shape[1], axis=1), data.shape[2], axis=2)
-    
-    data_norm = np.multiply(data - m_matrix, 1./std_matrix)
-    
-    return data_norm
+
+    m_matrix = np.repeat(np.repeat(m, spectrograms.shape[1], axis=1), spectrograms.shape[2], axis=2)
+    std_matrix = np.repeat(np.repeat(std, spectrograms.shape[1], axis=1), spectrograms.shape[2], axis=2)
+
+    normed = np.multiply(spectrograms - m_matrix, 1. / std_matrix)
+
+    return normed
+
 
 def create_pairs(train_data, train_labels, ref_data, num_classes):
     '''
@@ -33,7 +34,7 @@ def create_pairs(train_data, train_labels, ref_data, num_classes):
     pairs_left = []
     pairs_right = []
     pairs_labels = []
-    #for data, label in zip(train_data, train_labels):
+    # for data, label in zip(train_data, train_labels):
     for data, label in zip(train_data, train_labels):
         pairs_left.append(data)
         pairs_right.append(ref_data[label])
@@ -45,29 +46,30 @@ def create_pairs(train_data, train_labels, ref_data, num_classes):
         pairs_left.append(data)
         pairs_right.append(ref_data[random_label])
         pairs_labels.append(0)
-    
-    return np.array(pairs_left), np.array(pairs_right), np.array(pairs_labels) 
 
-train_data = np.load('./data/npy/TL/first_half_train_X.npy')
-train_labels = np.load('./data/npy/TL/first_half_train_Y.npy')
+    return np.array(pairs_left), np.array(pairs_right), np.array(pairs_labels)
 
-val_data = np.load('./data/npy/TL/first_half_val_X.npy')
-val_labels = np.load('./data/npy/TL/first_half_val_Y.npy')
 
-ref_data = np.load('./data/npy/TL/first_half_ref_X.npy')
-ref_labels = np.load('./data/npy/TL/first_half_ref_Y.npy')
+train_data = np.load('../../data/npy_archive/first_half_train_X.npy')
+train_labels = np.load('../../data/npy_archive/first_half_train_Y.npy')
 
-train_data_norm = normalize_spectrogram(train_data)
-val_data_norm = normalize_spectrogram(val_data)
-ref_data_norm = normalize_spectrogram(ref_data)
+val_data = np.load('../../data/npy_archive/first_half_val_X.npy')
+val_labels = np.load('../../data/npy_archive/first_half_val_Y.npy')
+
+ref_data = np.load('../../data/npy_archive/first_half_ref_X.npy')
+ref_labels = np.load('../../data/npy_archive/first_half_ref_Y.npy')
+
+train_data_norm = normalize_spectrograms(train_data)
+val_data_norm = normalize_spectrograms(val_data)
+ref_data_norm = normalize_spectrograms(ref_data)
 
 num_classes = ref_labels.shape[0]
 train_pairs_left, train_pairs_right, train_pairs_labels = create_pairs(train_data_norm, train_labels, ref_data_norm, num_classes)
 val_pairs_left, val_pairs_right, val_pairs_labels = create_pairs(val_data_norm, val_labels, ref_data_norm, num_classes)
 
-print train_pairs_left.shape
-print train_pairs_right.shape
-print train_pairs_labels.shape
+# print train_pairs_left.shape
+# print train_pairs_right.shape
+# print train_pairs_labels.shape
 
 ## Shuffle
 idx = np.random.permutation(train_pairs_left.shape[0])
@@ -82,9 +84,9 @@ tr_right_final = train_pairs_right_shuf.reshape(train_pairs_right_shuf.shape[0],
 val_left_final = val_pairs_left.reshape(val_pairs_left.shape[0], 1, 39, 482)
 val_right_final = val_pairs_right.reshape(val_pairs_right.shape[0], 1, 128, 128)
 
-# np.save('./data/npy/TL/train_pairs_data_left.npy', tr_left_final)
-# np.save('./data/npy/TL/train_pairs_data_right.npy', tr_right_final)
-# np.save('./data/npy/TL/train_pairs_labels.npy', train_pairs_labels_shuf)
-# np.save('./data/npy/TL/val_pairs_data_left.npy', val_left_final)
-# np.save('./data/npy/TL/val_pairs_data_right.npy', val_right_final)
-# np.save('./data/npy/TL/val_pairs_labels.npy', val_pairs_labels)
+# np.save('../../data/npy_archive/TL/train_pairs_data_left.npy', tr_left_final)
+# np.save('../../data/npy_archive/TL/train_pairs_data_right.npy', tr_right_final)
+# np.save('../../data/npy_archive/TL/train_pairs_labels.npy', train_pairs_labels_shuf)
+# np.save('../../data/npy_archive/TL/val_pairs_data_left.npy', val_left_final)
+# np.save('../../data/npy_archive/TL/val_pairs_data_right.npy', val_right_final)
+# np.save('../../data/npy_archive/TL/val_pairs_labels.npy', val_pairs_labels)
