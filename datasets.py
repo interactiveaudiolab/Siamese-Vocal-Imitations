85e9f4bef28b3f0b68b7c95d35ec6c2979ebae31
Brian Margolis <Brian.M.Margolis@gmail.com>
2018-06-21 16:09:49 -0500

    Add datasets for all imitations, all references, and all positive pairs

--- datasets.py
+++ datasets.py
@@ -6,6 +6,49 @@ from preprocessing import normalize_spectrograms
 from utils import load_npy
 
 
+class FineTuned(dataset.Dataset):
+    def __init__(self):
+        self.all_positives = self.get_positive_pairs()
+        self.pairs = []
+
+        self.reset()
+
+    def add_negative(self, i, r):
+        self.pairs.append([i, r, False])
+
+    def reset(self):
+        self.pairs = []
+        for i, r in self.all_positives:
+            self.pairs.append([i, r, True])
+
+    def __getitem__(self, index):
+        return self.pairs[index]
+
+    def __len__(self):
+        return len(self.pairs)
+
+    @staticmethod
+    def get_positive_pairs():
+        references = load_npy("references.npy")
+        references = normalize_spectrograms(references)
+        reference_labels = load_npy("reference_labels.npy")
+
+        imitations = load_npy("imitations.npy")
+        imitations = normalize_spectrograms(imitations)
+        imitation_labels = load_npy("imitation_labels.npy")
+
+        bar = Bar("Creating training pairs", max=len(references) * len(imitations))
+        pairs = []  # (imitation spectrogram, reference spectrogram)
+        for reference, reference_label in zip(references, reference_labels):
+            for imitation, imitation_label in zip(imitations, imitation_labels):
+                if reference_label == imitation_label:
+                    pairs.append([imitation, reference])
+                bar.next()
+        bar.finish()
+
+        return pairs
+
+
 class AllPositivesRandomNegatives(dataset.Dataset):
     def __init__(self):
         positives, negatives = self.get_all_training_pairs()
@@ -51,14 +94,79 @@ class AllPositivesRandomNegatives(dataset.Dataset):
         bar = Bar("Creating training pairs", max=len(references) * len(imitations))
         positive_pairs = []  # (imitation spectrogram, reference spectrogram)
         negative_pairs = []
+        # i = 0
         for reference, reference_label in zip(references, reference_labels):
             for imitation, imitation_label in zip(imitations, imitation_labels):
                 if reference_label == imitation_label:
                     positive_pairs.append([imitation, reference])
+                    # i += 1
                 else:
                     negative_pairs.append([imitation, reference])
 
+                # if i > 10:
+                #     return positive_pairs, negative_pairs
                 bar.next()
         bar.finish()
 
         return positive_pairs, negative_pairs
+
+
+class AllImitations(dataset.Dataset):
+    def __init__(self):
+        imitations = load_npy("imitations.npy")
+        imitations = normalize_spectrograms(imitations)
+        self.imitations = imitations
+
+    def __getitem__(self, index):
+        return self.imitations[index]
+
+    def __len__(self):
+        return len(self.imitations)
+
+
+class AllReferences(dataset.Dataset):
+    def __init__(self):
+        references = load_npy("references.npy")
+        references = normalize_spectrograms(references)
+        self.references = references
+
+    def __getitem__(self, index):
+        return self.references[index]
+
+    def __len__(self):
+        return len(self.references)
+
+
+class AllPositivePairs(dataset.Dataset):
+    def __init__(self):
+        self.all_positives = self.get_positive_pairs()
+        self.pairs = []
+        for i, r in self.all_positives:
+            self.pairs.append([i, r, True])
+
+    def __getitem__(self, index):
+        return self.pairs[index]
+
+    def __len__(self):
+        return len(self.pairs)
+
+    @staticmethod
+    def get_positive_pairs():
+        references = load_npy("references.npy")
+        references = normalize_spectrograms(references)
+        reference_labels = load_npy("reference_labels.npy")
+
+        imitations = load_npy("imitations.npy")
+        imitations = normalize_spectrograms(imitations)
+        imitation_labels = load_npy("imitation_labels.npy")
+
+        bar = Bar("Creating training pairs", max=len(references) * len(imitations))
+        pairs = []  # (imitation spectrogram, reference spectrogram)
+        for reference, reference_label in zip(references, reference_labels):
+            for imitation, imitation_label in zip(imitations, imitation_labels):
+                if reference_label == imitation_label:
+                    pairs.append([imitation, reference])
+                bar.next()
+        bar.finish()
+
+        return pairs
