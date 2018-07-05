2d599a655ddbb1c5c3fb235469b3c1e633b6334e
Brian Margolis <Brian.M.Margolis@gmail.com>
2018-07-05 15:59:23 -0500

    Update right tower architecture

--- models/transfer_learning.py
+++ models/transfer_learning.py
@@ -2,21 +2,39 @@ import torch.nn as nn
 
 
 class LeftTower(nn.Module):
-    def __init__(self):
+    def __init__(self, normalization=True):
         super(LeftTower, self).__init__()
 
         # left branch: vocal imitations
-        self.left_branch = nn.Sequential(
-            nn.Conv2d(1, 48, (6, 6)),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2),
-            nn.Conv2d(48, 48, (6, 6)),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2),
-            nn.Conv2d(48, 48, (6, 6)),
-            nn.ReLU(),
-            nn.MaxPool2d((1, 2), (1, 2)),
-        )
+        if normalization:
+            # left branch: vocal imitations
+            self.left_branch = nn.Sequential(
+                nn.Conv2d(1, 48, (6, 6)),
+                nn.BatchNorm2d(48, momentum=.01, eps=.001),
+                nn.ReLU(),
+                nn.MaxPool2d(2, 2),
+                nn.Conv2d(48, 48, (6, 6)),
+                nn.BatchNorm2d(48, momentum=.01, eps=.001),
+                nn.ReLU(),
+                nn.MaxPool2d(2, 2),
+                nn.Conv2d(48, 48, (6, 6)),
+                nn.BatchNorm2d(48, momentum=.01, eps=.001),
+                nn.ReLU(),
+                nn.MaxPool2d((1, 2), (1, 2)),
+            )
+        else:
+            # left branch: vocal imitations
+            self.left_branch = nn.Sequential(
+                nn.Conv2d(1, 48, (6, 6)),
+                nn.ReLU(),
+                nn.MaxPool2d(2, 2),
+                nn.Conv2d(48, 48, (6, 6)),
+                nn.ReLU(),
+                nn.MaxPool2d(2, 2),
+                nn.Conv2d(48, 48, (6, 6)),
+                nn.ReLU(),
+                nn.MaxPool2d((1, 2), (1, 2)),
+            )
 
     def forward(self, left, right):
         left_output = self.left_branch(left)
@@ -24,19 +42,53 @@ class LeftTower(nn.Module):
 
 
 class RightTower(nn.Module):
-    def __init__(self):
+    def __init__(self, normalization=True):
         super(RightTower, self).__init__()
-        # right branch: reference sounds
-        self.right_branch = nn.Sequential(
-            nn.Conv2d(1, 24, (5, 5)),
-            nn.ReLU(),
-            nn.MaxPool2d((2, 4), (2, 4)),
-            nn.Conv2d(24, 48, (5, 5)),
-            nn.ReLU(),
-            nn.MaxPool2d((2, 4), (2, 4)),
-            nn.Conv2d(48, 48, (5, 5)),
-            nn.ReLU(),
-        )
+        if normalization:
+            self.right_branch = nn.Sequential(
+                nn.Conv2d(1, 24, (5, 5)),
+                nn.BatchNorm2d(24, momentum=.01, eps=.001),
+                nn.ReLU(),
+                nn.MaxPool2d((2, 4), (2, 4)),
+
+                nn.Conv2d(24, 48, (5, 5)),
+                nn.BatchNorm2d(48, momentum=.01, eps=.001),
+                nn.ReLU(),
+                nn.MaxPool2d((2, 4), (2, 4)),
+
+                nn.Conv2d(48, 48, (5, 5)),
+                nn.BatchNorm2d(48, momentum=.01, eps=.001),
+                nn.ReLU(),
+
+                nn.Dropout(p=.5),
+                nn.Linear(48 * 25 * 2, 64),
+                nn.ReLU(),
+
+                nn.Dropout(p=.5),
+                nn.Linear(64, 10),
+                nn.Softmax()
+            )
+        else:
+            self.right_branch = nn.Sequential(
+                nn.Conv2d(1, 24, (5, 5)),
+                nn.ReLU(),
+                nn.MaxPool2d((2, 4), (2, 4)),
+
+                nn.Conv2d(24, 48, (5, 5)),
+                nn.ReLU(),
+                nn.MaxPool2d((2, 4), (2, 4)),
+
+                nn.Conv2d(48, 48, (5, 5)),
+                nn.ReLU(),
+
+                nn.Dropout(p=.5),
+                nn.Linear(48 * 25 * 2, 64),
+                nn.ReLU(),
+
+                nn.Dropout(p=.5),
+                nn.Linear(64, 10),
+                nn.Softmax()
+            )
 
     def forward(self, left, right):
         right_output = self.right_branch(right)
