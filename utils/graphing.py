ac5d00483f5138263d83aee6319ab1cfa8f4c0d2
Brian Margolis <Brian.M.Margolis@gmail.com>
2018-07-05 11:47:56 -0500

    Add denser ticks on y axes

--- utils/graphing.py
+++ utils/graphing.py
@@ -1,6 +1,7 @@
 import os
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 import utils.utils as utilities
 
@@ -9,11 +10,16 @@ def mrr_per_epoch(train_mrrs, val_mrrs, title="MRR vs. Epoch", xlabel='epoch'):
     plt.plot(train_mrrs, color='blue', label='training')
     plt.plot(val_mrrs, color='orange', label='validation')
     plt.legend()
+
     plt.ylabel('MRR')
+    plt.yticks(np.arange(0, 1.1, .1))
     plt.ylim(0, 1)
+
     plt.xlabel(xlabel)
+
     plt.suptitle(title)
     plt.title("Trial #{0}".format(utilities.get_trial_number()))
+
     filename = title_to_filename(title)
     plt.savefig(filename)
     plt.close()
@@ -23,8 +29,11 @@ def loss_per_epoch(train_loss, val_loss, title="Loss vs. Epoch"):
     plt.plot(train_loss, color='blue', label='training')
     plt.plot(val_loss, color='orange', label='validation')
     plt.legend()
+
     plt.ylabel('loss')
-    plt.ylim(0, 1)
+    plt.yticks(np.arange(0, 2.1, .1))
+    plt.ylim(0, 2)
+
     plt.xlabel('epoch')
     plt.suptitle(title)
     plt.title("Trial #{0}".format(utilities.get_trial_number()))
