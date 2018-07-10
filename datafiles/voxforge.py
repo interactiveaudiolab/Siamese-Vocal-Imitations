2a17a2e8c943332ad9022f400f35e99e1d6ae565
Brian Margolis <Brian.M.Margolis@gmail.com>
2018-07-10 16:49:55 -0500

    Call spectrograms spectrograms

--- datafiles/voxforge.py
+++ datafiles/voxforge.py
@@ -16,10 +16,9 @@ def path_to_fold_n(audio_path):
     return int(n[::-1]) - 1  # folds are 1 indexed in path names
 
 
-def calculate_spectrograms(per_language, n_batches):
+def calculate_spectrograms(per_language: int, n_batches: int):
     """
     Calculates normalized imitation and reference spectrograms and saves them as .npy files.
-    :param n_batches:
     """
     logger = logging.getLogger('logger')
     data_dir = os.path.join(os.environ['SIAMESE_DATA_DIR'], "voxforge")
@@ -65,12 +64,12 @@ class Voxforge:
         if recalculate_spectrograms:
             calculate_spectrograms(self.per_language, n_batches)
 
-        self.audio = np.empty(shape=[0, 39, 482])
+        self.spectrograms = np.empty(shape=[0, 39, 482])
         self.labels = np.array([])
         bar = Bar("Loading voxforge spectrograms from file", max=n_batches)
         for batch_n in range(n_batches):
             audio_batch = load_npy('voxforge_{0}.npy'.format(batch_n))
-            self.audio = np.concatenate([self.audio, audio_batch])
+            self.spectrograms = np.concatenate([self.spectrograms, audio_batch])
             label_batch = load_npy('voxforge_{0}_labels.npy'.format(batch_n))
             self.labels = np.concatenate([self.labels, label_batch])
             bar.next()
--- datasets/voxforge.py
+++ datasets/voxforge.py
@@ -8,20 +8,20 @@ class All(TowerData):
         super().__init__()
         self.labels = data.labels
         self.unique_labels = data.unique_labels
-        self.audio = data.audio
+        self.spectrograms = data.spectrograms
 
         if shuffle:
-            self.labels, self.audio = zip_shuffle(self.labels, self.audio)
+            self.labels, self.spectrograms = zip_shuffle(self.labels, self.spectrograms)
 
-        self.by_label = {u:[] for u in self.unique_labels}
+        self.by_label = {u: [] for u in self.unique_labels}
 
         n_train = int(.7 * data.per_language)
         n_validate = int(.3 * data.per_language)
 
         for unique_label in self.unique_labels:
-            for audio, label in zip(self.audio, self.labels):
+            for spectrogram, label in zip(self.spectrograms, self.labels):
                 if label == unique_label:
-                    self.by_label[label].append([audio, label])
+                    self.by_label[label].append([spectrogram, label])
 
         for unique_label in self.unique_labels:
             language = self.by_label[unique_label]
