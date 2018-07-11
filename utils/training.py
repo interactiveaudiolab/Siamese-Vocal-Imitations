c1ccc2a0860d4217267e44a31ccb927ec982106b
Brian Margolis <Brian.M.Margolis@gmail.com>
2018-07-11 12:02:27 -0500

    Fix tower training progress bar message

--- utils/training.py
+++ utils/training.py
@@ -50,7 +50,7 @@ def train_siamese_network(model: Siamese, data, objective, optimizer, n_epochs, 
 def train_tower(model: Tower, data: TowerData, objective, optimizer, n_epochs, use_cuda, batch_size=128):
     for epoch in range(n_epochs):
         train_data = DataLoader(data, batch_size=batch_size, num_workers=1)
-        bar = Bar("Training right tower, epoch {0}".format(epoch), max=len(train_data))
+        bar = Bar("Training tower, epoch {0}".format(epoch), max=len(train_data))
         batch_losses = np.zeros(len(train_data))
         for i, (audio, labels) in enumerate(train_data):
             # clear out the gradients
