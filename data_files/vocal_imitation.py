75f56a61186b96a0310c1e3671f179d5a2179c50
Brian Margolis <brian.m.margolis@gmail.com>
2018-08-01 17:14:01 -0500

    Update vocal imitation data files code for revised data set

--- data_files/vocal_imitation.py
+++ data_files/vocal_imitation.py
@@ -16,44 +16,29 @@ class VocalImitation(Datafiles):
         imitation_path = os.path.join(data_dir, "vocal_imitation/imitations")
         reference_path = os.path.join(data_dir, "vocal_imitation/references")
 
-        csv1 = os.path.join(data_dir, "vocal_imitation/category_name_lookup.csv")
-        csv2 = os.path.join(data_dir, "vocal_imitation/ref_filename_lookup.csv")
-
-        with open(csv1) as f1, open(csv2) as f2:
-            reader1 = csv.DictReader(f1)
-            reader2 = csv.DictReader(f2)
-            dict2 = dict((row['new_ref_filename'].replace("_reference.wav", ""), row['old_ref_filename']) for row in reader2)
-            canonical_lookup = {}
-            for row in reader1:
-                if row['new_category_name'] in dict2:
-                    canonical_lookup[row['old_category_name']] = dict2[row['new_category_name']]
-
         imitation_paths = preprocessing.recursive_wav_paths(imitation_path)
         reference_paths = preprocessing.recursive_wav_paths(reference_path)
 
         imitation_labels = {}
         for imitation in imitation_paths:
-            base_name = os.path.basename(imitation)
-            tokens = base_name.split("_")
-            label = '_'.join(tokens[2:])  # label is all tokens after the first two
-            label = label[:-4]  # trim off file extension
-            label = label[:label.rfind('-')]  # trim off number suffix
+            file_name = os.path.basename(imitation)
+            label = file_name[:3]
             imitation_labels[imitation] = label
 
         reference_labels = {}
         for path in reference_paths:
-            base_name = os.path.basename(os.path.dirname(path))
-            tokens = base_name.split("_")
-            label = '_'.join(tokens[2:])
-            if base_name in canonical_lookup:
-                is_canonical = canonical_lookup[base_name] == os.path.basename(path)
-            else:
-                is_canonical = False
+            category_name = os.path.basename(os.path.dirname(path))
+            file_name = os.path.basename(path)
+            label = category_name[:3]
+
+            is_canonical = 'perfect' in file_name.lower()
             reference_labels[path] = {"label": label,
                                       "is_canonical": is_canonical}
 
-        preprocessing.calculate_spectrograms(imitation_paths, imitation_labels, 'imitations', 'vocal_imitation', preprocessing.imitation_spectrogram)
-        preprocessing.calculate_spectrograms(reference_paths, reference_labels, 'references', 'vocal_imitation', preprocessing.reference_spectrogram)
+        preprocessing.calculate_spectrograms(imitation_paths, imitation_labels, 'imitations', 'vocal_imitation',
+                                             preprocessing.imitation_spectrogram)
+        preprocessing.calculate_spectrograms(reference_paths, reference_labels, 'references', 'vocal_imitation',
+                                             preprocessing.reference_spectrogram)
 
     @staticmethod
     def generate_occurrences(imitation_labels, reference_labels):
