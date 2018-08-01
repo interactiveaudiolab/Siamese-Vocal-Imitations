7f409422debd150ac1dd70b8d7311555712aa352
Brian Margolis <Brian.M.Margolis@gmail.com>
2018-08-01 17:26:35 -0500

    Save list of categories too

--- data_partitions/generics.py
+++ data_partitions/generics.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import pickle
 import sys
 
@@ -66,6 +67,19 @@ class Partitions:
         with open(location, 'wb') as f:
             pickle.dump(self.shuffled_imitation_indices, f, protocol=pickle.HIGHEST_PROTOCOL)
             pickle.dump(self.shuffled_category_indices, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+        categories = np.array(list(set([v['label'] for v in self.dataset.reference_labels])))
+
+        if not self.n_train_val_categories:
+            n_train_val = int(self.split.train_ratio * len(categories) + self.split.validation_ratio * len(categories))
+        else:
+            n_train_val = self.n_train_val_categories
+
+        categories = categories[self.shuffled_category_indices][:n_train_val]
+
+        category_list_path = os.path.join(os.path.dirname(location), 'categories.txt')
+        with open(category_list_path, 'w+') as f:
+            f.write("\n".join(categories))
 
     def construct_partition_arguments(self):
         imitations = self.dataset.imitations
